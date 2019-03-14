8cccedcf241ac307fbdf5e884a51f6ce22648ace
Naman Bhalla <namanbhalla1998@gmail.com>
2019-03-14 23:10:28 +0530

    Repository Object

---
+++ git_repository.py
@@ -0,0 +1,27 @@
+import os, configparser
+from utilities import *
+
+
+class GitRepository(object):
+    worktree, gitdir, conf = None, None, None
+
+    def __init__(self, path, force=False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception("Not a Git Repository %s" % path)
+
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+
+        elif not force:
+            raise Exception("Configuration File Missing")
+
+        if not force:
+            vers = int(self.conf.get("core", "repositoryformatversion"))
+            if vers != 0:
+                raise Exception("Unsupported repositoryformatversion %s" % vers)
---
+++ utilities.py
@@ -0,0 +1,65 @@
+import os, configparser
+from git_repository import GitRepository
+
+
+def repo_path(repo, *path):
+    return os.path.join(repo.gitdir, *path)
+
+
+def repo_dir(repo, *path, mkdir=False):
+    path = repo_path(repo, *path)
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            return path
+        else:
+            raise Exception("Not a directory %s" % path)
+
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
+
+
+def repo_file(repo, *path, mkdir=False):
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+
+def repo_default_config():
+    ret = configparser.ConfigParser()
+    ret.add_section("core")
+    ret.set("core", "repositoryformatversion", "0")
+    ret.set("core", "filemode", "false")
+    ret.set("core", "bare", "false")
+
+    return ret
+
+
+def repo_create(path):
+    repo = GitRepository(path, True)
+
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception("%s not a directory." % path)
+        if os.listdir(repo.worktree):
+            raise Exception("%s is not empty" % path)
+    else:
+        os.makedirs(repo.worktree)
+
+    assert (repo_dir(repo, "branches", mkdir=True))
+    assert (repo_dir(repo, "objects", mkdir=True))
+    assert (repo_dir(repo, "refs", "tags", mkdir=True))
+    assert (repo_dir(repo, "refs", "heads", mkdir=True))
+
+    with open(repo_file(repo, "description"), "w") as f:
+        f.write("Unnamed repository; edit this file 'description' to name the repository.\n")
+
+    with open(repo_file(repo, "HEAD"), "w") as f:
+        f.write("ref: refs/heads/master\n")
+
+    with open(repo_file(repo, "config"), "w") as f:
+        config = repo_default_config()
+        config.write(f)
+
+    return repo
